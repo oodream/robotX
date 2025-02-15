e94733ca9ca589067446e89c201e5723551c959b
Zuncheng <coolcoot@126.com>
2025-02-15 14:57:27 +0800

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,9 @@
 #!/bin/python
 
-print("start ros ai")
+def is_leap_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
 
+print("start ros ai")
